d704bc85bb2dab655130090b131d9d8407b4c4b9
Christian Cameron <christiancameron@Christians-MacBook-Pro.local>
2017-12-27 12:56:53 -0600

    Added CSV file parser to load websites

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # import libraries
+import csv
 import urllib2
 from bs4 import BeautifulSoup
 
@@ -15,7 +16,11 @@ def parse(url):
     avg_meta_len = 0
     
     #Get the page
-    page = urllib2.urlopen(url)
+    try:
+        page = urllib2.urlopen(url)
+    except:
+        return
+
     #Parse the website
     soup = BeautifulSoup(page, 'html.parser')
     
@@ -36,19 +41,48 @@ def parse(url):
     #Fetch all meta tags in the current website
     tags = soup.findAll(attrs={"name":"description"})
     for tag in tags:
-        #Find the length of the content part in the html meta tag
-        total_meta_len += len(str(tag['content'].encode('utf-8')))
         num_meta = num_meta + 1
+        #Find the length of the content part in the html meta tag
+        try:
+            total_meta_len += len(str(tag['content'].encode('utf-8')))
+        except:
+            pass
 
     #Print the number of meta tags
     print(url + ' has ' + str(num_meta) + ' meta name tags on the landing page')
-    avg_meta_len = total_meta_len / num_meta
+    try:
+        avg_meta_len = total_meta_len / num_meta
+    except:
+        avg_meta_len = 0
+
     print(url + ' has an average meta name tag length of ' + str(avg_meta_len) + ' on the landing page')
 
 
+print('Starting CSV file load process')
+sites = []
+#Load the csv file and store all websites in sites[]
+with open('top-500-websites.csv') as csvDataFile:
+    csvReader = csv.reader(csvDataFile)
+    for row in csvReader:
+        sites.append('https://www.' + row[1])
+print('CSV file loaded successfully')
+
+#Remove the head element from the list
+sites.pop(0)
+
+for site in sites:
+    print(site)
+    parse(site)
+
+print('Sites successfully scrapped')
 
 
-#Sample website to scrap
-parse('http://www.vimeo.com')
+
+
+
+
+
+
+
 
 
