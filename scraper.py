dea70cf7c2c75caef01378ccfff4503cb686f0c5
Christian Cameron <christiancameron@Christians-MacBook-Pro.local>
2017-12-26 20:58:52 -0600

    Added img counter for a website

--- scraper.py
+++ scraper.py
@@ -1,5 +1,31 @@
+# -*- coding: utf-8 -*-
 # import libraries
 import urllib2
 from bs4 import BeautifulSoup
+
+
+def scrap(url):
+    #Values we are keeping track of
+    num_images = 0
+    num_links = 0
+    num_meta = 0
+    len_meta = 0
+    
+    #Get the page
+    page = urllib2.urlopen(url)
+    
+    #Parse the website
+    soup = BeautifulSoup(page, 'html.parser')
+    
+    #Fetch all images in the current website
+    img=soup.findAll(['img'])
+    for i in img:
+        num_images = num_images + 1
+    
+    print(num_images)
+
+
+
+scrap('http://www.vimeo.com')
 
 
